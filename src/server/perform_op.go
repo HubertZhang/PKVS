5d26ac6a0229519e93dafb849a5bacb76dac01d2
JoeJ11 <andyjvan@hotmail.com>
2015-06-27 18:42:45 +0800

    Add syntax check logic. Hole in chain is not handled.

--- src/server/perform_op.go
+++ src/server/perform_op.go
@@ -11,21 +11,121 @@ func (self *Server) performOp(seq int, op Op) (bool, string) {
 }
 
 func (self *Server) performGET(seq int, op Op) (bool, string) {
+	list := []int {INSERT, UPDATE, DELETE}
+	seq, item := self.moveToTarget(list, op.Key)
+	if seq == 0 {
+		// Try to get an non-existent key
+		return false, ""
+	}
+
+	if seq == -1 {
+		// There is a hole in the chain
+		return false, ""
+	}
+
+	switch item.Op.Operation {
+	case INSERT, UPDATE:
+		return true, item.Op.Value
+	case DELETE:
+		return false, ""
+	}
+
 	return false, ""
 }
 
 func (self *Server) performINSERT(seq int, op Op) (bool, string) {
+	list := []int {INSERT, DELETE}
+	seq, item := self.moveToTarget(list, op.Key)
+	if seq == 0 {
+		return true, ""
+	}
+
+	if seq == -1 {
+		// There is a hole in the chain
+		return false, ""
+	}
+
+	switch item.Op.Operation {
+	case INSERT:
+		return false, item.Op.Value
+	case DELETE:
+		return true, ""
+	}
+
 	return false, ""
 }
 
 func (self *Server) performUPDATE(seq int, op Op) (bool, string) {
+	list := []int {INSERT, DELETE}
+	seq, item := self.moveToTarget(list, op.Key)
+	if seq == 0 {
+		// Try to update an non-existent key
+		return false, ""
+	}
+
+	if seq == -1 {
+		// There is a hole in the chain
+		return false, ""
+	}
+
+	switch item.Op.Operation {
+	case INSERT:
+		return true, ""
+	case DELETE:
+		return false, ""
+	}
+
 	return false, ""
 }
 
 func (self *Server) performDELETE(seq int, op Op) (bool, string) {
+	list := []int {INSERT, DELETE}
+	seq, item := self.moveToTarget(list, op.Key)
+	if seq == 0 {
+		// Try to delete an non-existent key
+		return false, ""
+	}
+
+	if seq == -1 {
+		// There is a hole in the chain
+		return false, ""
+	}
+
+	switch item.Op.Operation {
+	case INSERT:
+		return true, ""
+	case DELETE:
+		return false, ""
+	}
+
 	return false, ""
 }
 
-func (self *Server) moveToTarget(op_list []int) int {
-	return 0
+func (self *Server) moveToTarget(op_list []int, key string) (int, *Item) {
+	var pre_pos *Item = nil
+	var tem_pos *Item = self.tail
+	for true {
+		if tem_pos.SequenceNumber == 0 {
+			return 0, nil
+		}
+
+		flag := false
+		for _, op := range op_list {
+			if tem_pos.Op.Operation == op && tem_pos.Op.Key == key {
+				flag = true
+				break
+			}
+		}
+		if flag {
+			break
+		}
+
+		if pre_pos != nil && tem_pos.SequenceNumber != pre_pos.SequenceNumber - 1 {
+			return -1, nil
+		}
+		pre_pos = tem_pos
+		tem_pos = tem_pos.Next
+	}
+
+	return tem_pos.SequenceNumber, tem_pos
 }
