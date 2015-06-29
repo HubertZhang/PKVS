d0b97722e3b4871a401134853ef92c6373ffd666
Hubertzhang <hubert.zyk@gmail.com>
2015-06-29 16:09:11 +0800

    temp commit, CAN BE REBASED AT ANY TIME!!!!

--- compile.sh
+++ compile.sh
@@ -2,8 +2,4 @@
 
 export GOPATH="`pwd`"
 
-cd $GOPATH/src/paxos
-go install
-
-cd $GOPATH/src/server
-go install
+go install server
--- src/server/main.go
+++ src/server/main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"net/http"
+	"paxos"
+	"fmt"
+	"os"
+	"strconv"
+	"encoding/gob"
 )
+var paxos_peer *paxos.Paxos
 
 func main () {
+	gob.Register(Op{})
+	if len(os.Args) != 2 {
+		fmt.Print("Usage: "+os.Args[0] +" <node id>")
+		return
+	}
+	me, _ := strconv.Atoi(os.Args[1])
+	peers, port, err := load_config()
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+//	peers[me-1] = "10000"
+
+	paxos_peer = paxos.Make(peers, me-1, nil)
+	getServer(paxos_peer, me-1)
+	fmt.Println("here")
 	http.HandleFunc("/kv/insert", handleInsert)
 	http.HandleFunc("/kv/get", handleGet)
 	http.HandleFunc("/kv/delete", handleDelete)
@@ -13,6 +35,10 @@ func main () {
 	http.HandleFunc("/kvman/dump", handleDump)
 	http.HandleFunc("/kvman/count", handleCount)
 	http.HandleFunc("/kvman/halt", handleHalt)
+	fmt.Println(":"+strconv.Itoa(port))
 
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
