43c2b159bf9ca31a52650c1c6fe49f103413817f
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2015-06-30 18:58:45 +0800

    finished normal consistency test

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ import requests
 import re
 import os
 import json
-import random
+from  random import randint
 import time
 import subprocess
 import threading
@@ -75,10 +75,78 @@ def dump(me):
     r = requests.get(servers_man[me]+"dump", params={})
     return r.json(), r.elapsed.total_seconds()
 
+def organized_dump(me):
+    r, t = dump(me)
+    d = {}
+    for p in r:
+        d[p[0]] = p[1]
+    return d
+
 def count(me):
     r = requests.get(servers_man[me]+'countkey', params={})
     return r.json(), r.elapsed.total_seconds()
 
-def normal_consistency_test(): #random
+word_pool = ["key1", "_key2", "^%!@#$%^&*()key3", "{key4", "key5_+=",
+             "key6-=_+-[", "key7测试", "{key8=", "_key9\'\"", "]key10\\|",
+             "12421", "aslf\rjhalgha", "657468sv\0ca", "18726\a(^&(^(",
+             "0chp3\"`", ")*HPB", "啦啦啦", "+++", "~!@#GX", "{\ndAFqw}"]
+
+def normal_consistency_test(n): #random
+    read_config()
     start_all()
-    
+    m = len(word_pool)-1
+    d = {}
+    for i in range(0,n):
+        act = randint(1,4)
+        print "------ %s: %s ------" %(str(i),str(act))
+        if act == 1:
+            if len(d) > 0 and randint(1,5) > 1:
+                key = d.keys()[randint(0,len(d)-1)]
+            else:
+                key = word_pool[randint(0,m)]
+            r, t = get(randint(1,3), key=key)
+            if (not r['success'] and d.has_key(key)) or (not d.has_key(key) and r['success']):
+                print "error on operation #%s: GET key=%s" %(str(i), key)
+                break
+            elif r['success'] and r['value'] != d[key]:
+                print "error on operation #%s: GET key=%s conflict with value1=%s, value2=%s" %(str(i), key, r['value'],d[key])
+                break
+        elif act == 2:
+            key = word_pool[randint(0,m)]
+            value = word_pool[randint(0,m)]
+            r, t = insert(randint(1,3), key=key, value=value)
+            if (not r['success'] and not d.has_key(key)) or (r['success'] and d.has_key(key)):
+                print "error on operation #%s: INSERT conflict, key=%s" %s(str(i), key)
+                break
+            if r['success']:
+                d[key] = value
+        elif act == 3:
+            key = word_pool[randint(0,m)]
+            value = word_pool[randint(0,m)]
+            r, t = update(randint(1,3), key=key, value=value)
+            if (not r['success'] and d.has_key(key)) or (not d.has_key(key) and r['success']):
+                print "error on operation #%s: UPDATE conflict, key=%s" %s(str(i), key)
+                break
+            if r['success']:
+                d[key] = value
+        else:
+            key = word_pool[randint(0,m)]
+            r, t = delete(randint(1,3), key=key)
+            if (not r['success'] and d.has_key(key)) or (not d.has_key(key) and r['success']):
+                print "error on operation #%s: DELETE conflict, key=%s" %s(str(i), key)
+                break
+            if r['success']:
+                d.pop(key)
+    d1 = organized_dump(1)
+    d2 = organized_dump(2)
+    d3 = organized_dump(3)
+    if d == d1 and d1 == d2 and d2 == d3:
+        print "Perfect"
+    else:
+        print "Inconsistency!"
+        print "d",d
+        print "d1",d1
+        print "d2",d2
+        print "d3",d3
+    print "Finish"
+    shutdown_all()
